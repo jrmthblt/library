97a45b97cfb0e99313d82e37cc8ae5db3f059fb2
arnaudf92 <arnaudf92@gmail.com>
2018-04-30 22:03:49 +0200

    Added some clustering tries: - skmeans - kmeans - self organizing map (kohonen map)

--- arnaud/processDataset.R
+++ arnaud/processDataset.R
@@ -6,18 +6,18 @@
 ## INIT
 
 #columns2keep
-c2k <- c("volumeInfo.title",
-         "volumeInfo.publisher",
-         "volumeInfo.description",
-         "volumeInfo.categories",
-         "volumeInfo.language",
-         "volumeInfo.subtitles",
-         "volumeInfo.authors")
+#c2k <- c("volumeInfo.title",
+#          "volumeInfo.publisher",
+#          "volumeInfo.description",
+#          "volumeInfo.categories",
+#          "volumeInfo.language",
+#          "volumeInfo.subtitles",
+#          "volumeInfo.authors")
 
 infile <- "dataset.csv"
 dataset_df <- read.csv2(infile, header=TRUE, sep=";")
 
-dataset_df <- dataset_df[-c(29,32,37,39,171,1158,1182,1597,1623),]
+dataset_df <- dataset_df[-c(29,32,37,39,171,1158,1182,1597,1623),] # Nettoyage de certains enregistrements problématiques
 
 ## Prepare DTM
 #install.packages('tm')
@@ -27,20 +27,20 @@ library(tm)
 library(stringi)
 library(stringr)
 
-#Corpus initial
+#Initial corpus
 documents <- Corpus(VectorSource(dataset_df$volumeInfo.description))
 
-#Désaccentuation
+#Remove chars with accents
 accent <- function(x) stri_trans_general(x, "Latin-ASCII") # cela signifie qu'on remplace un caractère encodé en Latin1 par son équivalent le plus proche en ASCII, il n'y a par exemple pas de caractères accentués en ASCII
 documents <- tm_map(documents, content_transformer(accent))
 
-#filtrage ponctuation et nombres
+#Remove numbers and punctuation
 documents <- tm_map(documents, content_transformer(gsub), pattern = "[^a-zA-Z]", replacement = " ")
 
-#Minuscules
+#Put in lowercase
 documents <- tm_map(documents, content_transformer(tolower))
 
-#Suppression des stopwords (mots-outils)
+#Stopwords removal
 #stopwords_fr <- stopwords("french")
 stopwords_fr <- sapply(stopwords("french"),accent)
 documents <- tm_map(documents, removeWords, stopwords_fr)
@@ -48,7 +48,10 @@ documents <- tm_map(documents, removeWords, stopwords_fr)
 stopwords_fr2 = c(stopwords_fr,'a','h','lundi','mardi','mercredi','jeudi','vendredi','samedi','dimanche',
                  'etre','apres','selon','comme','alors','tout','tous','faire','depuis','encore',
                  'peut','doit','mieux',"un","deux","trois","quatre","cinq","six","sept","huit","neuf","dix",
-                 'tant','ainsi')
+                 'tant','ainsi','livre','oeuvre','ouvrage','aussi','autre','fait','entre','plus','tout',
+                 'toute','auteur','bien','dont','roman','comment','petit','petite','grand','grande',
+                 'etc','annee'
+                 )
 stopwords_fr2 = setdiff(stopwords_fr2, c("pas")) # 'pas' est inclus dans les stopword, on trouve que c'est un peu dommage alors on le retire de la liste
 documents <- tm_map(documents, removeWords, stopwords_fr2)
 
@@ -67,46 +70,134 @@ documents <- tm_map(documents, content_transformer(gsub), pattern = "^\\s+", rep
 dtm <- DocumentTermMatrix(documents)
 
 #Elagage (minimum 20 mots)
-minfreq <- findFreqTerms(dtm, 30)
+minfreq <- findFreqTerms(dtm, 20)
 length(minfreq)
 
 dtm <- removeSparseTerms(
-  DocumentTermMatrix(documents, control=list(dictionary= minfreq, weighting = function(x) weightTfIdf(x, normalize = FALSE)))
+  DocumentTermMatrix(documents, control=list(dictionary= minfreq, weighting = function(x) weightTfIdf(x, normalize = TRUE)))
   ,
-  0.9)
+  0.88)
+
+dtm2 <- removeSparseTerms(
+  DocumentTermMatrix(documents, control=list(dictionary= minfreq, weighting = function(x) weightTf(x)))
+  ,
+  0.88)
+
 
 #inspect(dtm)
 #dim(dtm)
 #documents[1]$content
+#documents_nonstem[1231]$content
 
-df <- data.frame(as.matrix(dtm), stringsAsFactors=FALSE)
-m <- as.matrix(dtm)
 
 rowTotals <- apply(dtm , 1, sum) #Find the sum of words in each Document
-dtm.new   <- dtm[rowTotals> 0, ]           #remove all docs without words
+dtm.new   <- dtm[rowTotals> 0, ] #remove all docs without words
 
-##Classification
-#install.packages('caret')
-library(caret)
+rowTotals2 <- apply(dtm2 , 1, sum) #Find the sum of words in each Document
+dtm.new2   <- dtm2[rowTotals> 0, ] #remove all docs without words
+
+
+### Clustering
 #install.packages('skmeans')
 library(skmeans)
+library(cluster)
+#install.packages('proxy')
+library(proxy)
 
-#Non-supervisé
+
 #http://michael.hahsler.net/SMU/CSE7337/ et http://michael.hahsler.net/SMU/CSE7337/install/tm.R
 #http://edutechwiki.unige.ch/fr/Tutoriel_tm_text_mining_package
 #https://stackoverflow.com/questions/13944252/remove-empty-documents-from-documenttermmatrix-in-r-topicmodels
 #https://stackoverflow.com/questions/24459246/text-analysis-using-lda-and-tm-in-r
-## do document clustering
-sk <- skmeans(x=dtm.new, 6)
+#
+
+### don't forget to normalize the vectors so Euclidean makes sense
+norm_eucl <- function(m) m/apply(m, MARGIN=1, FUN=function(x) sum(x^2)^.5)
+m_norm <- norm_eucl(as.matrix(dtm.new))
+
+set.seed(123)
 
 
-table(sk$cluster)
+#Silouhette method for finding the optimal number of clusters
 
-### show clusters using the first 2 principal components
-plot(prcomp(m_norm)$x, col=cl$cl)
+# Compute the average silhouette width for 
+# k = 2 to k = 20 (skmeans)
+k.max <- 20
+sil <- rep(0,k.max)
+for(i in 2:k.max){
+  k.res <- skmeans(dtm.new, i)
+  ss <- silhouette(k.res$cluster, proxy::dist(as.matrix(dtm.new), method="cosine"))
+  sil[i] <- mean(ss[, 3])
+}
 
-findFreqTerms(dtm[cl$cluster==1], 50)
-#inspect(reuters[which(cl$cluster==1)])
+# Plot the average silhouette width (skmeans)
+plot(1:k.max, sil, type = "b", pch = 19, 
+     frame = FALSE, xlab = "Number of clusters k")
+abline(v = which.max(sil), lty = 2)
+
+
+# Compute the average silhouette width for 
+# k = 2 to k = 20 (kmeans)
+k.max <- 20
+sil2 <- rep(0,k.max)
+for(i in 2:k.max){
+  k.res2 <- kmeans(m_norm, i, nstart=50,iter.max = 15)
+  ss2 <- silhouette(k.res2$cluster, dist(m_norm))
+  sil2[i] <- mean(ss2[, 3])
+}
+
+# Plot the average silhouette width (kmeans)
+plot(1:k.max, sil2, type = "b", pch = 19, 
+     frame = FALSE, xlab = "Number of clusters k")
+abline(v = which.max(sil2), lty = 2)
+
+
+# cluster into x (optimal) clusters - skmeans
+cl <- skmeans(dtm.new, 16)
+table(cl$cluster)
+# show clusters using the first 2 principal components
+plot(prcomp(m_norm)$x, col=cl$cluster)
+
+
+# cluster into x (optimal) clusters - kmeans
+cl2 <- kmeans(m_norm, 16)
+table(cl2$cluster)
+# show clusters using the first 2 principal components
+plot(prcomp(m_norm)$x, col=cl2$cluster)
+
+
+## Self organizing map (som) - Kohonen map
+#install.packages('kohonen')
+library(kohonen)
+
+data_train_matrix <- as.matrix(scale(m_norm))
+# Create the SOM Grid - you generally have to specify the size of the 
+# training grid prior to training the SOM. Hexagonal and Circular 
+# topologies are possible
+som_grid <- somgrid(xdim = 6, ydim=6, topo="hexagonal")
+# Finally, train the SOM, options for the number of iterations,
+# the learning rates, and the neighbourhood are available
+som_model <- som(data_train_matrix, 
+                 grid=som_grid, 
+                 rlen=2000, 
+                 alpha=c(0.05,0.01), 
+                 keep.data = TRUE)
+
+plot(som_model, type="changes")
+plot(som_model, type="count", main="Node Counts")
+plot(som_model, type="dist.neighbours", main = "SOM neighbour distances")
+
+
+### it seems the optimal number of cluster is more or less the number of remaining words in DTM... ###
+### Instability of the clustering with many tries ###
+
+
+
+### Classification
+
+#install.packages('caret')
+library(caret)
+
 
 
 ## Write final dataset csv
